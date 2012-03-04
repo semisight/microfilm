07c9dd0deb472dc93545efcf4f056fbb59f95dd5
Alex Bogart <billyman3@gmail.com>
2012-03-04 13:38:31 -0800

    added simple styles.

--- micro.py
+++ micro.py
@@ -1,16 +1,24 @@
+#!/usr/bin/env python
 from __future__ import division
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 ###Views
+
+#Login to facebook and Twitter
 @app.route('/')
 def index():
-	pass
+	#If login is correct, should redirect to display()
+	return render_template('index.html')
 
 @app.route('/<name>')
 def hi(name):
 	return 'hi, {0}!'.format(name)
 
+@app.route('/<_>/<month>/<day>/<year>')
+def display(_, month, day, year):
+	return "Stella is estudpido!"
+
 if __name__=='__main__':
-	app.run()
+	app.run(debug=True)
