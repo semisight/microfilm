97d8681019ba6933912b3dc66b230e72a949d409
Alex Bogart <billyman3@gmail.com>
2012-03-06 16:05:54 -0800

    Breaking now?

--- micro.py
+++ micro.py
@@ -88,8 +88,8 @@ def display(month, day, year):
 	date_begin = (year, month, day, 0, 0, 0)
 	date_end = (year, month, day, 23, 59, 59)
 
-	dates = {#'sice=': str(timegm(date_begin)),
-			 'until=': str(timegm(date_end))}
+	dates = {'since=': timegm(date_begin),
+			 'until=': timegm(date_end)}
 
 	resp = facebook.get('/me/home', data=dates)
 
